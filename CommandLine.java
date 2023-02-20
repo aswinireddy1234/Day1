84b6925400a6b10671e632a33477b32dc64b97d8
aswinireddy1234 <chegireddyaswinireddy@gmail.com>
2023-02-20 14:16:23 +0530

    Sub branch

---
+++ CommandLine.java
@@ -0,0 +1,18 @@
+
+public class CommandLine {
+  public static void main(String[] args) {
+    int sum = 0;
+    int invalidCount = 0;
+
+    for (int i = 0; i < args.length; i++) {
+      try {
+        sum += Integer.parseInt(args[i]);
+      } catch (NumberFormatException e) {
+        invalidCount++;
+      }
+    }
+
+    System.out.println("Sum of valid integers: " + sum);
+    System.out.println("Number of invalid integers: " + invalidCount);
+  }
+}
